65e6528e10ea00d96bb2e7aa6962b9e6395bd68e
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2020-11-23 13:04:04 -0600

    Added new pages

--- app.py
+++ app.py
@@ -81,6 +81,7 @@ def index():
         privilegeResult=privilegeResult,accountPrivilegeResult=accountPrivilegeResult,
         relationPrivilegeResult=relationPrivilegeResult, tablesResult=tablesResult, has_on_Result=has_on_Result)
 
+
 @app.route('/showCreateUserForm')
 def showCreateUserForm():
     
@@ -92,7 +93,9 @@ def showCreateUserForm():
 
     return render_template('showCreateUserForm.html', roleIdNoResult=roleIdNoResult)
 
+    
 
+    
 @app.route('/createUserForm', methods=['GET', 'POST'])
 def createUserForm():
     
@@ -146,6 +149,7 @@ def createUserRoleForm():
 @app.route('/showCreatePrivilegesForm')
 def showCreatePrivilegesForm():
     
+
     return render_template('showCreatePrivilegesForm.html')
 
 
@@ -153,20 +157,25 @@ def showCreatePrivilegesForm():
 def createPrivilegesForm():
     
     # fetch entered value from the form
-    privilegeID = request.form['privilegeID']
     privilegeType = request.form['privilegeType']
+    IdNo = request.form['IdNo']
+    functionalityName = request.form['functionalityName']
+    functionalityDescription = request.form['functionalityDescription']
+    roleIdNumber = request.form['roleIdNumber']
+    privilegeIdNumber = request.form['privilegeIdNumber']
 
-    # Execution of query
-    query = "INSERT INTO PRIVILEGES values ('"+privilegeID+"', '"+privilegeType+"');"
+    if(privilegeType == "Account_Privilege_Type"):
+        query = "INSERT INTO ACCOUNT_PRIVILEGES values ('"+IdNo+"', '"+functionalityName+"','"+functionalityDescription+"','"+roleIdNumber+"','"+privilegeIdNumber+"')";
+    else:
+        query = "INSERT INTO RELATION_PRIVILEGES values ('"+IdNo+"', '"+functionalityName+"','"+functionalityDescription+"','"+roleIdNumber+"','"+privilegeIdNumber+"')";
+
+
     connectionCursor.execute(query)
 
-    # fetch the reults
     roleIdNoResult = connectionCursor.fetchall()
     connection.commit()
 
     return redirect('/')
-
-
 
 
 
@@ -202,6 +211,143 @@ def createTablesForm():
     connection.commit()
 
     return redirect('/')
+
+
+
+
+
+
+@app.route('/showTransaction5')
+def showTransaction5():
+    
+    return render_template('showTransaction5.html')
+
+@app.route('/transaction5form', methods=['GET', 'POST'])
+def transaction5form():
+    
+    # fetch entered value from the form
+    roleIdNo = request.form['roleIdNo']
+    IdNo = request.form['IdNo']
+
+    # Execution of query
+    query = "UPDATE USER_ACCOUNT set Role_Id='"+roleIdNo+"' where IdNo='"+IdNo+"'"
+    connectionCursor.execute(query)
+
+    # fetch the reults
+    roleIdNoResult = connectionCursor.fetchall()
+    connection.commit()
+
+    return redirect('/')
+
+
+@app.route('/showTransaction6')
+def showTransaction6():
+    
+
+    return render_template('showTransaction6.html')
+
+@app.route('/transaction6form', methods=['GET', 'POST'])
+def transaction6form():
+    
+    # fetch entered value from the form
+    roleIdNo = request.form['roleIdNo']
+    accountIdNo = request.form['accountIdNo']
+
+    # Execution of query
+    query = "UPDATE ACCOUNT_PRIVILEGES set Role_Id='"+roleIdNo+"' where Account_Id='"+accountIdNo+"'"
+    connectionCursor.execute(query)
+
+    # fetch the reults
+    roleIdNoResult = connectionCursor.fetchall()
+    connection.commit()
+
+    return redirect('/')
+
+
+@app.route('/showTransaction7')
+def showTransaction7():
+    
+
+    return render_template('showTransaction7.html')
+    
+@app.route('/transaction7form', methods=['GET', 'POST'])
+def transaction7form():
+    
+    # fetch entered value from the form
+    roleIdNo = request.form['roleIdNo']
+    tableIdNo = request.form['tableIdNo']
+
+    # Execution of query
+    query = "UPDATE HAS_ON set Role_Id='"+roleIdNo+"' where Table_Id='"+tableIdNo+"' and Privilege_Id='P02'"
+    connectionCursor.execute(query)
+
+    # fetch the reults
+    result1 = connectionCursor.fetchall()
+    print(result1)
+    connection.commit()
+
+    return render_template('showTransaction7.html', result1=result1)
+
+
+
+
+@app.route('/showTransaction8a')
+def showTransaction8a():
+    
+
+    return render_template('showTransaction8a.html')
+    
+@app.route('/transaction8formRole', methods=['GET', 'POST'])
+def transaction8formRole():
+    
+    # fetch entered value from the form
+    roleIdNo = request.form['roleIdNo']
+
+    # Execution of query
+    query1 = "SELECT Account_Functionality_name from ACCOUNT_PRIVILEGES , USER_ROLE where ACCOUNT_PRIVILEGES.Role_id=USER_ROLE.Role_id and USER_ROLE.Role_id='"+roleIdNo+"'"
+    connectionCursor.execute(query1)
+
+    result1 = connectionCursor.fetchall()
+
+    query2="SELECT Relation_Functionality_name from USER_ROLE, HAS_ON , RELATION_PRIVILEGES where USER_ROLE.Role_id='"+roleIdNo+"' and USER_ROLE.Role_id = HAS_ON.Role_id and HAS_ON.Relation_Id=RELATION_PRIVILEGES.Relation_Id"
+    connectionCursor.execute(query2)
+    
+    # fetch the reults
+    result2 = connectionCursor.fetchall()
+    connection.commit()
+
+    return render_template('showTransaction8a.html', result1=result1, result2=result2)
+    
+
+
+@app.route('/showTransaction8b')
+def showTransaction8b():
+    
+
+    return render_template('showTransaction8b.html')
+
+@app.route('/transaction8formAccount', methods=['GET', 'POST'])
+def transaction8formAccount():
+    
+    # fetch entered value from the form
+    accountIdNo = request.form['accountIdNo']
+    print(accountIdNo)
+    # Execution of query
+    query1 = "SELECT Account_Functionality_name from account_privileges, user_account, user_role where IdNo='"+accountIdNo+"' and ACCOUNT_PRIVILEGES.Role_id=USER_ROLE.Role_id and USER_ACCOUNT.Role_Id=USER_ROLE.Role_id"
+    connectionCursor.execute(query1)
+
+    result1 = connectionCursor.fetchall()
+
+    query2="SELECT Relation_Functionality_name from USER_ACCOUNT, USER_ROLE, HAS_ON , RELATION_PRIVILEGES where IdNo='"+accountIdNo+"' and USER_ACCOUNT.Role_Id=USER_ROLE.Role_id and USER_ROLE.Role_id = HAS_ON.Role_id and HAS_ON.Relation_Id=RELATION_PRIVILEGES.Relation_Id"
+    connectionCursor.execute(query2)
+    
+    # fetch the reults
+    result2 = connectionCursor.fetchall()
+    connection.commit()
+    print(result1)
+    return render_template('showTransaction8b.html', result1=result1, result2=result2)
+
+
 
 
 
